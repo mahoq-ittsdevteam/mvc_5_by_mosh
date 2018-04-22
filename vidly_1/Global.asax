5d7f1d7be1be9f4abda86c14a0ffe97a4fd6c114
Mohammed Hoq <mahoq07@gmail.com>
2018-04-22 20:58:59 +1000

    1st commit

---
+++ vidly_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vidly_1.MvcApplication" Language="C#" %>
---
+++ vidly_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vidly_1.Startup))]
+namespace vidly_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
