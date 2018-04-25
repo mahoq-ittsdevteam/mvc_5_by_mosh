47ca1147d3e8df5c751c1f05104c0e52e0035d68
Mohammed Hoq <mahoq07@gmail.com>
2018-04-25 23:17:03 +1000

    First commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            //return View(movie);
+
+            //return HttpNotFound();
+
+            //return Content("Hello World");
+
+            //return new EmptyResult();
+
+            //return RedirectToAction("index", "Home", new { page = 1});
+
+            /*
+                ViewData["Movie"] = movie;
+                ViewBag.Movie = movie;
+                return View();
+            */
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&softBy={1}",pageIndex, sortBy));
+        }
+
+    }
+}
